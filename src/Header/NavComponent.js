3a03ff2c6b3bab6cd48d1ba6e8ca2e450bd5e401
rmehtije <rmehtije@gmail.com>
2022-06-03 17:59:49 +0300

    home work

--- src/Header/NavComponent.js
+++ src/Header/NavComponent.js
@@ -5,20 +5,23 @@ import cities from './cities.json';
 export default function NavComponent() {
 
     const [ selectedCity, setSelectedCity ] = useState({city: "Choose city"});
- 
+    const [ units, setUnits ] = useState('');
+
     return (
         <Navbar variant="dark" bg="dark" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="#home">Weather App</Navbar.Brand>
+                <Navbar.Brand href="#home">Weather App {units}</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbar-dark-example" />
                 <Navbar.Collapse id="navbar-dark-example">
                 <Nav>
+                    <Nav.Link onClick={() => setUnits('C')}>C</Nav.Link>
+                    <Nav.Link onClick={() => setUnits('F')}>F</Nav.Link>
                     <NavDropdown
-                    id="nav-dropdown-dark-example"
-                    title={selectedCity.city}
-                    menuVariant="dark"
+                        id="nav-dropdown-dark-example"
+                        title={selectedCity.city}
+                        menuVariant="dark"
                     >
-                        
+
                         {cities.map( city => 
                             <NavDropdown.Item onClick={() => setSelectedCity(city)} href="#action/3.1">{city.city}</NavDropdown.Item>
                         )}
