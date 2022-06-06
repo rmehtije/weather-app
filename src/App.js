6c25c0195d948dff53dd183f5f788e0dcff4ba0d
rmehtije <rmehtije@gmail.com>
2022-06-06 20:38:03 +0300

    lesson 4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import HeaderComponent from './Header/HeaderComponent';
+import dataTypes from './Header/type.json';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { google_api_key } from './keys';
+
+const containerStyle = {
+  width: '800px',
+  height: '600px'
+};
+
+const center = {
+  lat: 59.4370,
+  lng: 24.7536
+};
+
 
 function App() {
 
-  function handleOnSubmit (event) {
+  const [map, setMap] = React.useState(null)
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: google_api_key
+  });
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+
+  function handleOnSubmit(event) {
     // setSelectedCity(city);
     event.preventDefault();
     const city = event.target.city.value;
     const unit = event.target.unit.value;
-    const dataType = event.target.dataType;
+    let selectedTypes = [];
+
+    for (const dtype of event.target.dataType) {
+      if (dtype.checked === true) {
+        selectedTypes.push(dtype.value);
+      }
+    }
+
+    let exclude = dataTypes.filter(dtype => !selectedTypes.includes(dtype.value));
+
+    console.log(exclude);
+    // console.log(selectedTypes);
     const language = event.target.language.value;
     console.log(city);
     console.log(unit);
-    console.log(dataType);
     console.log(language);
-}
+  }
 
 
   return (
@@ -24,7 +67,22 @@ function App() {
       <Row>
         <Col>
           <HeaderComponent firstName="Rasim" handleOnSubmitForm={handleOnSubmit} />
-          
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {isLoaded ? (
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={6}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
+            >
+              { /* Child components, such as markers, info windows, etc. */}
+              <></>
+            </GoogleMap>
+          ) : <></>}
         </Col>
       </Row>
     </Container>
--- src/Header/FormComponent.js
+++ src/Header/FormComponent.js
@@ -15,6 +15,7 @@ export default function FormComponent(props) {
                             key={dtype.value}
                             type="checkbox"
                             name="dataType"
+                            defaultChecked={props.dataType.value === dtype.value}
                             label={dtype.label}
                             value={dtype.value}
                         />
@@ -22,7 +23,7 @@ export default function FormComponent(props) {
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="language">
                     <Form.Label>Choose language</Form.Label>
-                    <Form.Select name="language" aria-label="Default select example">
+                    <Form.Select defaultValue={props.language} name="language" aria-label="Default select example">
                         <option>Choose language</option>
                         {['en', 'fi', 'ru'].map(language =>
                             <option key={language}>{language}</option>
@@ -31,30 +32,35 @@ export default function FormComponent(props) {
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="city">
                     <Form.Label>Choose city</Form.Label>
-                    <Form.Select name="city" aria-label="Default select example">
+                    <Form.Select defaultValue={props.selectedCity.name} name="city" aria-label="Default select example">
                         <option>Open this select menu</option>
                         {cities.map(city =>
-                            <option key={city.city}>{city.city}</option>
+                            <option key={city.name}>{city.name}</option>
                         )}
                     </Form.Select>
                 </Form.Group>
                 <Form.Group controlId="unit">
                     <Form.Label>Choose unit</Form.Label>
-                    <Form.Check
-                        name="unit"
-                        type='radio'
-                        label={`C`}
-                        value='C'
-                    />
-                    <Form.Check
-                        name="unit"
-                        type='radio'
-                        label={`F`}
-                        value='F'
-                    />
+                    {['standard', 'metric', 'imperial'].map(unit =>
+                        <Form.Check
+                            key={unit}
+                            name="unit"
+                            type='radio'
+                            defaultChecked={props.unit === unit}
+                            label={unit}
+                            value={unit}
+                        />
+                    )}
                 </Form.Group>
                 <Button variant="primary" type="submit">Submit</Button>
             </Form>
         </Container>
     )
+}
+
+FormComponent.defaultProps = {
+    selectedCity: cities.find(city => city.name === 'Tallinn'),
+    language: "en",
+    dataType: dataTypes.find(type => type.label === 'Daily'),
+    unit: "metric",
 }
